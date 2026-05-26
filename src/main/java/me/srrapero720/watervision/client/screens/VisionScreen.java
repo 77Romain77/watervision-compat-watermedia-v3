eba1d5bed166d3dbd49e47dc5fef43503ebce162
77Romain77 <113790889+77Romain77@users.noreply.github.com>
2026-05-26 18:35:10 +0200

    Port video screen to WaterMedia v3 API

--- src/main/java/me/srrapero720/watervision/client/screens/VisionScreen.java
+++ src/main/java/me/srrapero720/watervision/client/screens/VisionScreen.java
@@ -1,5 +1,6 @@
 package me.srrapero720.watervision.client.screens;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import me.srrapero720.watervision.WaterVision;
 import me.srrapero720.watervision.WaterVisionClient;
 import me.srrapero720.watervision.client.render.TextureWrapper;
@@ -11,8 +12,12 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraftforge.fml.loading.FMLLoader;
-import org.watermedia.api.player.PlayerAPI;
-import org.watermedia.api.player.videolan.VideoPlayer;
+import org.watermedia.api.media.MRL;
+import org.watermedia.api.media.MediaAPI;
+import org.watermedia.api.media.engines.ALEngine;
+import org.watermedia.api.media.engines.GLEngine;
+import org.watermedia.api.media.players.FFMediaPlayer;
+import org.watermedia.api.media.players.MediaPlayer;
 
 import java.net.URI;
 import java.text.DateFormat;
@@ -28,20 +33,28 @@ public class VisionScreen extends Screen {
         FORMAT.setTimeZone(TimeZone.getTimeZone("GMT-00:00"));
     }
 
+    private final URI uri;
+    private final int volume;
+    private final float speed;
     private final boolean stretch;
     private final boolean controls;
     private final boolean exit;
-    // PLAYER
-    private final VideoPlayer videoPlayer;
-    private final TextureWrapper textureWrapper;
 
-    // STATE
+    private final MRL mrl;
+    private MediaPlayer videoPlayer;
+    private TextureWrapper textureWrapper;
+    private boolean failedToCreatePlayer;
+    private boolean released;
+
     private Status status = Status.OPENING_GAME;
     private final FadeBackground gameBackground;
     private final FadeBackground videoBackground;
 
     public VisionScreen(final URI uri, final int volume, final float speed, final boolean stretch, final float gameFadeDuration, final float videoFadeDuration, final boolean controls, final boolean exit) {
         super(Component.literal("WaterVision"));
+        this.uri = uri;
+        this.volume = Mth.clamp(volume, 0, 100);
+        this.speed = Mth.clamp(speed, 0.1f, 3f);
         this.stretch = stretch;
         this.controls = controls;
         this.exit = exit;
@@ -50,27 +63,75 @@ public class VisionScreen extends Screen {
         this.videoBackground = new FadeBackground(videoFadeDuration);
         this.videoBackground.forceFadeIn();
 
-        this.videoPlayer = new VideoPlayer(PlayerAPI.getFactory(), Minecraft.getInstance());
-        this.videoPlayer.setVolume(Mth.clamp(volume, 0, 100));
-        this.videoPlayer.setSpeed(Mth.clamp(speed, 0.1f, 3f));
-
-        this.textureWrapper = new TextureWrapper(this.videoPlayer.texture());
-        Minecraft.getInstance().getTextureManager().register(TEXTURE, this.textureWrapper);
-        this.videoPlayer.startPaused(uri);
+        this.mrl = MediaAPI.getMRL(uri);
         Minecraft.getInstance().getSoundManager().pause();
+    }
+
+    private void tryCreatePlayer() {
+        if (this.videoPlayer != null || this.failedToCreatePlayer || !this.mrl.ready()) {
+            return;
+        }
+
+        if (this.mrl.hasError()) {
+            this.failedToCreatePlayer = true;
+            WaterVision.LOGGER.error("Failed to load media resource: {}", this.uri, this.mrl.exception());
+            this.status = Status.CLOSING_VIDEO;
+            return;
+        }
+
+        this.videoPlayer = MediaAPI.createPlayer(this.mrl, this::createGfxEngine, this::createSfxEngine);
+        if (this.videoPlayer == null) {
+            this.failedToCreatePlayer = true;
+            WaterVision.LOGGER.error("WaterMedia v3 failed to create a player for: {}", this.uri);
+            this.status = Status.CLOSING_VIDEO;
+            return;
+        }
+
+        this.videoPlayer.volume(this.volume);
+        this.videoPlayer.speed(this.speed);
+        this.videoPlayer.repeat(false);
+
+        this.textureWrapper = new TextureWrapper(() -> (int) this.videoPlayer.texture());
+        Minecraft.getInstance().getTextureManager().register(TEXTURE, this.textureWrapper);
+        this.videoPlayer.startPaused();
+    }
+
+    private GLEngine createGfxEngine() {
+        return new GLEngine.Builder(Thread.currentThread(), Minecraft.getInstance())
+                .setGenTexture(GlStateManager::_genTexture)
+                .setBindTexture((target, texture) -> GlStateManager._bindTexture(texture))
+                .setTexParameter(GlStateManager::_texParameter)
+                .setPixelStore(GlStateManager::_pixelStore)
+                .setDelTexture(GlStateManager::_deleteTexture)
+                .build();
+    }
+
+    private ALEngine createSfxEngine() {
+        return ALEngine.buildDefault();
+    }
+
+    private void pollVideoFrame() {
+        if (this.videoPlayer instanceof FFMediaPlayer ffMediaPlayer) {
+            ffMediaPlayer.pollVideoFrame();
+        }
     }
 
     @Override
     public void render(final GuiGraphics guiGraphics, final int pMouseX, final int pMouseY, final float partialTick) {
+        this.tryCreatePlayer();
+
         this.gameBackground.render(guiGraphics, this.width, this.height, this.status != Status.CLOSING_GAME, partialTick);
 
-        if (this.status == Status.OPENING_VIDEO || this.status == Status.CLOSING_VIDEO) {
-            this.videoPlayer.preRender();
-            if (this.stretch) {
-                WaterVisionClient.internal$blit(guiGraphics, TEXTURE, 1, 0, 0, 0, 0, this.width, this.height);
-            } else {
-                final AspectRatioDimension dim = this.render$getAspectRatio(this.width, this.height, this.videoPlayer.width(), this.videoPlayer.height());
-                WaterVisionClient.internal$blit(guiGraphics, TEXTURE, 1, dim.x, dim.y, 0, 0, dim.width, dim.height);
+        if (this.videoPlayer != null && (this.status == Status.OPENING_VIDEO || this.status == Status.CLOSING_VIDEO)) {
+            this.pollVideoFrame();
+
+            if (this.videoPlayer.texture() != 0 && this.videoPlayer.width() > 0 && this.videoPlayer.height() > 0) {
+                if (this.stretch) {
+                    WaterVisionClient.internal$blit(guiGraphics, TEXTURE, 1, 0, 0, 0, 0, this.width, this.height);
+                } else {
+                    final AspectRatioDimension dim = this.render$getAspectRatio(this.width, this.height, this.videoPlayer.width(), this.videoPlayer.height());
+                    WaterVisionClient.internal$blit(guiGraphics, TEXTURE, 1, dim.x, dim.y, 0, 0, dim.width, dim.height);
+                }
             }
         }
 
@@ -78,19 +139,26 @@ public class VisionScreen extends Screen {
             this.videoBackground.render(guiGraphics, this.width, this.height, this.status == Status.CLOSING_VIDEO, partialTick);
         }
 
-        if (this.status == Status.OPENING_GAME || this.status == Status.CLOSING_VIDEO || this.status == Status.CLOSING_GAME || this.videoPlayer.isBuffering() || this.videoPlayer.isLoading()) {
+        if (this.shouldRenderLoadingIcon()) {
             this.render$loadingIcon(guiGraphics, partialTick);
         }
 
-        // DEBUG
-        if (!FMLLoader.isProduction()) {
-            if (!this.videoPlayer.isSafeUse()) return;
-            guiGraphics.drawString(this.font, String.format("State: %s", this.videoPlayer.getStateName()), 0, (this.height / 2) - 12, 0xFFFFFF);
-            guiGraphics.drawString(this.font, String.format("Time: %s (%s) / %s (%s)", FORMAT.format(new Date(this.videoPlayer.getTime())), this.videoPlayer.getTime(), FORMAT.format(new Date(this.videoPlayer.getDuration())), this.videoPlayer.getDuration()), 0, (this.height / 2), 0xFFFFFF);
-            guiGraphics.drawString(this.font, String.format("Media Duration: %s (%s)", FORMAT.format(new Date(this.videoPlayer.getMediaInfoDuration())), this.videoPlayer.getMediaInfoDuration()), 0, (this.height / 2) + 12, 0xFFFFFF);
+        if (!FMLLoader.isProduction() && this.videoPlayer != null) {
+            guiGraphics.drawString(this.font, String.format("State: %s", this.videoPlayer.status().name()), 0, (this.height / 2) - 12, 0xFFFFFF);
+            guiGraphics.drawString(this.font, String.format("Time: %s (%s) / %s (%s)", FORMAT.format(new Date(this.videoPlayer.time())), this.videoPlayer.time(), FORMAT.format(new Date(this.videoPlayer.duration())), this.videoPlayer.duration()), 0, (this.height / 2), 0xFFFFFF);
+            guiGraphics.drawString(this.font, String.format("Media Duration: %s (%s)", FORMAT.format(new Date(this.videoPlayer.duration())), this.videoPlayer.duration()), 0, (this.height / 2) + 12, 0xFFFFFF);
             guiGraphics.drawString(this.font, String.format("Orchestrator Status: %s", this.status.name()), 0, (this.height / 2) + 24, 0xFFFFFF);
             guiGraphics.drawString(this.font, String.format("Video Size: %sx%s", this.videoPlayer.width(), this.videoPlayer.height()), 0, (this.height / 2) + 36, 0xFFFFFF);
         }
+    }
+
+    private boolean shouldRenderLoadingIcon() {
+        return this.status == Status.OPENING_GAME
+                || this.status == Status.CLOSING_VIDEO
+                || this.status == Status.CLOSING_GAME
+                || this.videoPlayer == null
+                || this.videoPlayer.buffering()
+                || this.videoPlayer.loading();
     }
 
     private void render$loadingIcon(GuiGraphics graphics, float partialTick) {
@@ -122,15 +190,17 @@ public class VisionScreen extends Screen {
 
     @Override
     public void tick() {
+        this.tryCreatePlayer();
+
         switch (this.status) {
             case OPENING_GAME -> {
-                if (this.gameBackground.isFadedIn() && this.videoPlayer.isSafeUse() && this.videoPlayer.isReady()) {
+                if (this.gameBackground.isFadedIn() && this.videoPlayer != null && this.videoPlayer.canPlay()) {
                     this.status = Status.OPENING_VIDEO;
-                    this.videoPlayer.play();
+                    this.videoPlayer.resume();
                 }
             }
             case OPENING_VIDEO -> {
-                if (this.videoBackground.isFadedOut() && (this.videoPlayer.isEnded() || this.videoPlayer.isStopped() || this.videoPlayer.isBroken())) {
+                if (this.videoPlayer != null && this.videoBackground.isFadedOut() && (this.videoPlayer.ended() || this.videoPlayer.stopped() || this.videoPlayer.error())) {
                     this.status = Status.CLOSING_VIDEO;
                 }
             }
@@ -141,7 +211,8 @@ public class VisionScreen extends Screen {
             }
             case CLOSING_GAME -> {
                 if (this.gameBackground.isFadedOut()) {
-                    this.onClose();
+                    this.closeAndRelease();
+                    super.onClose();
                 }
             }
         }
@@ -154,18 +225,32 @@ public class VisionScreen extends Screen {
 
     @Override
     public void onClose() {
-        if (this.videoPlayer.isSafeUse() && this.videoPlayer.isPlaying()) {
-            this.videoPlayer.stop();
-            return;
-        }
         if (this.status != Status.CLOSING_GAME) {
+            if (this.videoPlayer != null && !this.videoPlayer.stopped() && !this.videoPlayer.ended() && !this.videoPlayer.error()) {
+                this.videoPlayer.stop();
+            }
+            this.status = Status.CLOSING_VIDEO;
             return;
         }
-        Minecraft.getInstance().getSoundManager().resume();
-        this.videoPlayer.release();
+
+        this.closeAndRelease();
         super.onClose();
     }
 
+    private void closeAndRelease() {
+        if (this.released) {
+            return;
+        }
+
+        Minecraft.getInstance().getSoundManager().resume();
+
+        if (this.videoPlayer != null) {
+            this.videoPlayer.release();
+            this.videoPlayer = null;
+        }
+
+        this.released = true;
+    }
 
     public record AspectRatioDimension(int x, int y, int width, int height) { }
 
