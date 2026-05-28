65aab6fb67410e9b279b55fadecf3f48b85e15e1
77Romain77 <113790889+77Romain77@users.noreply.github.com>
2026-05-29 00:44:50 +0200

    Reduce volume overlay duration to one second

--- src/main/java/me/srrapero720/watervision/client/screens/VisionScreen.java
+++ src/main/java/me/srrapero720/watervision/client/screens/VisionScreen.java
@@ -26,10 +26,10 @@ import java.net.URI;
 import java.util.function.Supplier;
 
 public class VisionScreen extends Screen {
-    private static final String BUILD_TAG = "cinematic-ui-dynamic-tips-debug";
+    private static final String BUILD_TAG = "cinematic-ui-volume-1s-debug";
     private static final ResourceLocation TEXTURE = ResourceLocation.tryBuild("watervision", "video_texture");
     private static final int TIPS_AUTO_HIDE_TICKS = 200;
-    private static final int VOLUME_OVERLAY_TICKS = 40;
+    private static final int VOLUME_OVERLAY_TICKS = 20;
     private static final int VOLUME_STEP = 5;
     private static final int SKIP_HOLD_TICKS = 40;
 
