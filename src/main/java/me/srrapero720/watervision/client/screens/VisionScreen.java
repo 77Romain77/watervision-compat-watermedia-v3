c7d32fbba382d9cbec68fa595ba2726842011435
77Romain77 <113790889+77Romain77@users.noreply.github.com>
2026-05-29 17:27:14 +0200

    Test tag update

--- src/main/java/me/srrapero720/watervision/client/screens/VisionScreen.java
+++ src/main/java/me/srrapero720/watervision/client/screens/VisionScreen.java
@@ -26,7 +26,7 @@ import java.net.URI;
 import java.util.function.Supplier;
 
 public class VisionScreen extends Screen {
-    private static final String BUILD_TAG = "cinematic-ui-early-recovery-10t-debug";
+    private static final String BUILD_TAG = "cinematic-ui-early-recovery-10t-debug2";
     private static final ResourceLocation TEXTURE = ResourceLocation.tryBuild("watervision", "video_texture");
     private static final int TIPS_AUTO_HIDE_TICKS = 200;
     private static final int VOLUME_OVERLAY_TICKS = 20;
