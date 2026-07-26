e8481acdab4b5b0791dbbf8397651982e441f7a0
77Romain77 <113790889+77Romain77@users.noreply.github.com>
2026-07-26 23:12:13 +0200

    Recover cinematic audio mute state from active screen

--- src/main/java/me/srrapero720/watervision/mixins/impl/MinecraftMixin.java
+++ src/main/java/me/srrapero720/watervision/mixins/impl/MinecraftMixin.java
@@ -1,6 +1,8 @@
 package me.srrapero720.watervision.mixins.impl;
 
 import me.srrapero720.watervision.VisionOverlay;
+import me.srrapero720.watervision.client.audio.CinematicAudioMute;
+import me.srrapero720.watervision.client.screens.VisionScreen;
 import net.minecraft.client.Minecraft;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,8 +17,17 @@ public class MinecraftMixin {
     @Shadow private volatile boolean pause;
 
     @Inject(method = "runTick", at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft;pause:Z", opcode = Opcodes.ISTORE, shift = At.Shift.AFTER))
-    public void inject$pause(final boolean pRenderLevel, CallbackInfo ci) {
+    public void inject$pause(final boolean pRenderLevel, final CallbackInfo ci) {
         VisionOverlay.onClientPause(this.pause);
 
+        final Minecraft minecraft = (Minecraft) (Object) this;
+        final boolean cinematicScreenActive = minecraft.screen instanceof VisionScreen;
+        final boolean stateChanged = CinematicAudioMute.setActive(cinematicScreenActive);
+
+        // Safety net for unexpected screen replacement/disconnection paths where
+        // VisionScreen.closeAndRelease() was not reached.
+        if (stateChanged && !cinematicScreenActive) {
+            minecraft.getSoundManager().resume();
+        }
     }
 }
